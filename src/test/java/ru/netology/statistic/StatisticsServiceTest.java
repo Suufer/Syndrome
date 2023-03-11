209769b603deb819f36ba9178ec93d2739c334a3
Suufer <lyapota231@gmail.com>
2023-03-11 21:27:28 +0300

    Commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -29,4 +29,34 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxWithCurrentIncomes() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindMaxWhenFirstElementIsMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {5, 4, 3, 2, 1};
+        long expected = 5;
+        long actual = service.findMax(incomes);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindMaxWhenLastElementIsMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {1, 2, 3, 4, 5};
+        long expected = 5;
+        long actual = service.findMax(incomes);
+        assertEquals(expected, actual);
+    }
 }
